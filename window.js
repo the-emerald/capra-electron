8dfb0ef1856ce1536991117eeee6ec3441293a5b
Anson <git@anson-cheung.com>
2020-02-11 11:13:40 +0000

    Fix UI and outputs

--- window.js
+++ window.js
@@ -1,32 +1,27 @@
 $(() => {
     const neon = require('capra-neon');
 
-    // $('#text-input').bind('input propertychange', function() {
-    //     const text = this.value
-    //
-    //     const md5 = crypto.createHash('md5').update(text, 'utf8').digest('hex')
-    //     $('#md5-output').text(md5)
-    //
-    //     const sha1 = crypto.createHash('sha1').update(text, 'utf8').digest('hex')
-    //     $('#sha1-output').text(sha1)
-    //
-    //     const sha256 = crypto.createHash('sha256').update(text, 'utf8').digest('hex')
-    //     $('#sha256-output').text(sha256)
-    //
-    //     const sha512 = crypto.createHash('sha512').update(text, 'utf8').digest('hex')
-    //     $('#sha512-output').text(sha512)
-    // });
-
     $('#submit').bind('click', function () {
         const depth = parseInt($("#depth-input").val());
         const time = parseInt($("#length-input").val());
         console.log(depth);
         console.log(time);
+        if (isNaN(depth) || isNaN(time)) {
+            $('#ndl-output').text("Invalid input");
+            return
+        }
+        try {
+            const ndl = neon.ndlDemo(depth, time);
+            if (ndl === 0) {
+                $('#ndl-output').text("Decompression");
+            }
+            else {
+                $('#ndl-output').text(ndl);
+            }
+        }
+        catch (e) {
+            $('#ndl-output').text("Invalid input");
+        }
 
-        const ndl = neon.ndlDemo(depth, time);
-
-        $('#ndl-output').text(ndl)
     })
-
-    $('#text-input').focus() // focus input box
 });
